2a6fa15b6bb258084bde8219bfcb2cde64410aed
Isaac <isaac1180@aol.com>
2020-10-02 12:51:00 -0400

    fixed error with navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+
+function NavBar(props) {
+    
+
+    return (
+        <>
+            <h1>Welcome to my portfolio.</h1> 
+        </>
+    )
+}
+
+export default NavBar;
