7acbf77e3dde91f818feae62858bc3a42f597358
Isaac <isaac1180@aol.com>
2020-10-06 10:56:05 -0400

    fixed navlink with proper link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,12 +10,13 @@ function NavBar(props) {
           <div className="collapse navbar-collapse vertically-centered" id="navbarNav">
             <ul className="navbar-nav">
              <li className="nav-item">
-                <NavLink to="/login" className="nav-link" >Portfolio</NavLink>
+                <NavLink to="/" className="nav-link" >Portfolio</NavLink>
              </li>
            
              <li className="nav-item" >
-                 <NavLink to="/register" className="nav-link">Projects</NavLink>
-             </li>  
+                 <NavLink to="/projects" className="nav-link">Projects</NavLink>
+             </li>
+
             </ul>
           </div>
        </nav>
