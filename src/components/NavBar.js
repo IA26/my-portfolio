e36b5d47d85181ad99d6418835ad8b6a9c2a7043
Isaac <isaac1180@aol.com>
2020-10-06 11:45:46 -0400

    added navbar link to head

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,15 +6,19 @@ function NavBar(props) {
     
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
-          <div className="collapse navbar-collapse vertically-centered" id="navbarNav">
-            <ul className="navbar-nav">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="collapse navbar-collapse vertically-centered">
+            <ul className="navbar-nav mr-auto list-unstyled">
              <li className="nav-item">
                 <NavLink to="/" className="nav-link" >Portfolio</NavLink>
              </li>
            
              <li className="nav-item" >
                  <NavLink to="/projects" className="nav-link">Projects</NavLink>
+             </li>
+
+             <li className="nav-item">
+                <NavLink to="/contact" className="nav-link">Contact</NavLink>
              </li>
 
             </ul>
