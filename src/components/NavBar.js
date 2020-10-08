1cb6308eed57d0a9207b072e8504c3dd9241ab03
Isaac <isaac1180@aol.com>
2020-10-08 11:48:53 -0400

    trying to add different section of nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,21 +6,23 @@ function NavBar(props) {
     
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+          <div> 
+            {/* <ul>
+                <li>
+                    <NavLink>Homepage</NavLink>
+                </li>    
+            </ul> */}
+          </div> 
           <div className="collapse navbar-collapse vertically-centered">
             <ul className="navbar-nav ml-auto list-unstyled">
-             <li className="nav-item">
-                <NavLink to="/" className="nav-link" >Portfolio</NavLink>
-             </li>
-           
-             <li className="nav-item" >
-                 <NavLink to="/projects" className="nav-link">Projects</NavLink>
-             </li>
+                <li className="nav-item" >
+                    <NavLink to="/projects" className="nav-link">Projects</NavLink>
+                </li>
 
-             <li className="nav-item">
-                <NavLink to="/contact" className="nav-link">Contact Me</NavLink>
-             </li>
-
+                <li className="nav-item">
+                   <NavLink to="/contact" className="nav-link">Contact Me</NavLink>
+                </li>
             </ul>
           </div>
        </nav>
