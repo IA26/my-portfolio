2f6983c6b3f4a1ecbde1a0a8604a963fd8d1e7ea
Isaac <isaac1180@aol.com>
2020-10-08 11:54:05 -0400

    fixed navbar, added left side to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,18 @@ function NavBar(props) {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-          <div> 
-            {/* <ul>
-                <li>
-                    <NavLink>Homepage</NavLink>
-                </li>    
-            </ul> */}
-          </div> 
           <div className="collapse navbar-collapse vertically-centered">
+            <ul className="navbar-nav list-unstyled">
+                <li>
+                    <NavLink to="/" className="nav-link">Portfolio</NavLink>
+                </li>
+            </ul>
+
+
+
+
+
+
             <ul className="navbar-nav ml-auto list-unstyled">
                 <li className="nav-item" >
                     <NavLink to="/projects" className="nav-link">Projects</NavLink>
@@ -30,13 +34,3 @@ function NavBar(props) {
 }
 
 export default NavBar;
-
-
-const reverse = (string) => { 
-    let reversed = ''
-
-    for (let character of string) {
-        reversed = character + reversed;
-    }
-   return reversed; 
-}
