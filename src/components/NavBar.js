35bdd022c5d13ac69b54c9b22b0db82a70bee336
Isaac <isaac1180@aol.com>
2020-10-09 15:09:57 -0400

    fixed css with background colour

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,26 +6,24 @@ function NavBar(props) {
     
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <nav className="navbar navbar-expand-lg navbar-dark" id="bg-colour">
           <div className="collapse navbar-collapse vertically-centered">
             <ul className="navbar-nav list-unstyled">
-                <li>
-                    <NavLink to="/" className="nav-link">Portfolio</NavLink>
+                <li class="nav-item">
+                    <NavLink to="/" className="nav-link" activeClassName="">Portfolio</NavLink>
                 </li>
             </ul>
 
 
 
 
-
-
             <ul className="navbar-nav ml-auto list-unstyled">
                 <li className="nav-item" >
-                    <NavLink to="/projects" className="nav-link">Projects</NavLink>
+                    <NavLink to="/projects" className="nav-link" activeClassName="">Projects</NavLink>
                 </li>
 
                 <li className="nav-item">
-                   <NavLink to="/contact" className="nav-link">Contact Me</NavLink>
+                   <NavLink to="/contact" className="nav-link" activeClassName="">Contact Me</NavLink>
                 </li>
             </ul>
           </div>
