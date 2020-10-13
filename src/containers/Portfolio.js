8f7b33b33db57e4f8039d18a90c966591ffc2d16
Isaac <isaac1180@aol.com>
2020-10-13 10:02:49 -0400

    testing main portfolio and removed ids

---
+++ src/containers/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Portfolio(props) {
+    
+
+    return (
+        <div className="div-content">      
+          <h1>Hello there</h1>
+          <h2>Welcome to my portfolio</h2> 
+        </div> 
+    )
+}
+
+export default Portfolio; 
