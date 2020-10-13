bc9e9a68b0766ffcbe79eecb3cb500d9759e4f2b
Isaac <isaac1180@aol.com>
2020-10-13 10:01:33 -0400

    trying main page gradient

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="main-page">
         <div id="content-wrap"> 
           <NavBar/>
            <Switch>
