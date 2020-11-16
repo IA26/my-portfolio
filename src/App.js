6a9d3450718e10c954f9c6153324aade8b9bbed5
Isaac <isaac1180@aol.com>
2020-11-16 10:47:43 -0500

    removed social page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App" id="main-page">
         <div id="content-wrap"> 
           <NavBarPage/>
-           <Switch>
+           <Switch> 
               <Route exact path="/" render component={ Portfolio }/>
            </Switch>
         </div> 
