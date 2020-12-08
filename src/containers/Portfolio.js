fcd61603b25e43e4b91f8a488183bcc9f50b6ccf
Isaac <isaac1180@aol.com>
2020-12-08 14:48:57 -0500

    added h2 tag

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -6,6 +6,7 @@ function Portfolio(props) {
     return (
         <div className="portfolio-content">      
           <h1>Welcome to my portfolio</h1>
+          <h2>This is my portfolio</h2>
          
         </div> 
     )
