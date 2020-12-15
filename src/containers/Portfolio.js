b8e7bd6892d7a316461adfddd306b8656fba45f0
Isaac <isaac1180@aol.com>
2020-12-15 16:51:19 -0500

    changed h2

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -6,7 +6,8 @@ function Portfolio(props) {
     return (
         <div className="portfolio-content">      
           <h1>Welcome to my portfolio</h1>
-          <h2>My name is Isaac, and I'm a Full-Stack developer</h2>
+          <h2>Hello, my name is Isaac, and I am a Full-stack developer.</h2>
+          <h3></h3> 
          
         </div> 
     )
