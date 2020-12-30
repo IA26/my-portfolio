ef796064844cde6cc7d93395ee1946214bb3d119
Isaac <isaac1180@aol.com>
2020-12-30 18:03:45 -0500

    added anchor for github link

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -10,6 +10,10 @@ function Portfolio(props) {
           <h3>I'm an avid enjoyer of lifting weights, and programming.</h3> 
           <h3>Below I've added some of my projects that I've worked on.</h3>
           
+
+          <ul>
+            <a href="https://github.com/IA26/covidgains-front-end"> Covid-gains front-end</a>
+          </ul>
         </div> 
     )
 }
