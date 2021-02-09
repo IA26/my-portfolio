9444127006e72089780cc794765de19adf881073
Isaac <isaac1180@aol.com>
2021-02-09 10:46:44 -0500

    adding content to skills page

---
+++ src/containers/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Skills(props) {
+    
+
+    return (
+        <>
+            <h1>Here are my lists of skills: </h1> 
+            <ul>
+                <li>Ruby on Rails</li>
+            </ul> 
+        </>
+    )
+}
+
+export default Skills;
