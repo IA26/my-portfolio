25fd2dfd4f4af89a53e65948ddd8992dbb60aab9
Isaac <isaac1180@aol.com>
2021-02-24 20:44:44 -0500

    added contact container

---
+++ src/containers/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+MDBNavbar, MDBNavLink, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBIcon, MDBContainer
+} from "mdbreact";
+
+function Contact(props) {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Contact
