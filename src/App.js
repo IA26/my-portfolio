721640d1de021a6d21d0c2cae727fb657102764a
Isaac <isaac1180@aol.com>
2021-02-24 20:47:16 -0500

    added details to contact container and imported to app js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBarPage from './components/NavBarPage'
 import Portfolio from './containers/Portfolio'
 import Skills from './containers/Skills'
+import Contact from './containers/Contact'
 import { Route, Switch } from "react-router";
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
            <Switch> 
               <Route exact path="/" render component={ Portfolio }/>
               <Route exact path="/skills" render component={ Skills } /> 
+              <Route exact path="/contact" render component={ Contact } /> 
            </Switch>
         </div> 
         <footer id="footer">
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,7 +6,7 @@ MDBNavbar, MDBNavLink, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBIcon, MDBCon
 function Contact(props) {
     return (
         <>
-            
+             <h3>If you would like to contact me, check out my <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/">linkedIn</a></h3> 
         </>
     )
 }
