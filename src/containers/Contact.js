78364985e37ca13a6b993ebce324ed36bdef8700
Isaac <isaac1180@aol.com>
2021-03-01 22:01:33 -0500

    removed imports on contact container

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,7 +1,4 @@
 import React from "react";
-import {
-MDBNavbar, MDBNavLink, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBIcon, MDBContainer
-} from "mdbreact";
 
 function Contact(props) {
     return (
