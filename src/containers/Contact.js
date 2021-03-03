e32ea84238b62088f04eb4305db85ee4fa81144e
Isaac <isaac1180@aol.com>
2021-03-03 17:42:06 -0500

    social links

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,7 +6,19 @@ import {
 function Contact(props) {
     return (
         <>
-             <h3>If you would like to contact me, check out my <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/">linkedIn</a></h3> 
+        <MDBContainer>
+            <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/" className="social-icon hover">
+              <MDBIcon fab icon="linkedin-in" />
+            </a>
+           
+            <a href="https://github.com/IA26" className="social-icon hover" >
+              <MDBIcon fab icon="github" />
+            </a>
+
+            <a href="https://isaac1180a.medium.com/" className="social-icon hover" >
+              <MDBIcon fab icon="medium-m" />
+            </a>
+        </MDBContainer> 
         </>
     )
 }
