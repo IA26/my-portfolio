13f76c5c18a97adf2ff82c2cf9bb8f3806f3b94b
Isaac <isaac1180@aol.com>
2021-03-10 19:36:58 -0500

    added text area and import for contact me page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
- MDBIcon, MDBContainer
+ MDBIcon, MDBContainer, MDBInput
 } from "mdbreact";
 
 function Contact(props) {
@@ -8,19 +8,41 @@ function Contact(props) {
        <>
        <div className="contact-content">
            <h1 id="contact-id">Contact me</h1>
-        <MDBContainer>
-            <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/" className="social-icon hover">
-              <MDBIcon fab icon="linkedin-in" />
-            </a>
-           
-            <a href="https://github.com/IA26" className="social-icon hover" >
-              <MDBIcon fab icon="github" />
-            </a>
 
-            <a href="https://isaac1180a.medium.com/" className="social-icon hover" >
-              <MDBIcon fab icon="medium-m" />
-            </a>
-        </MDBContainer> 
+
+
+
+
+
+
+        <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">
+            Basic textarea
+            </label>
+            <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="5"
+            />
+        </div>
+
+
+         <div className="logo-container">
+           <MDBContainer>
+             <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/" className="social-icon hover">
+                 <MDBIcon fab icon="linkedin-in" />
+             </a>
+           
+             <a href="https://github.com/IA26" className="social-icon hover" >
+                 <MDBIcon fab icon="github" />
+             </a>
+
+             <a href="https://isaac1180a.medium.com/" className="social-icon hover" >
+                 <MDBIcon fab icon="medium-m" />
+             </a>
+           </MDBContainer> 
+       </div>   
+       
      </div>
     </>
     )
