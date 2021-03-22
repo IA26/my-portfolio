e2a9335107959d90279fe913908d3a120b2041b7
Isaac <isaac1180@aol.com>
2021-03-21 20:37:24 -0400

    wiped all of navbar

--- src/components/NavBarPage.js
+++ src/components/NavBarPage.js
@@ -6,50 +6,50 @@ MDBNav, MDBNavLink, MDBNavbarBrand, MDBNavbarNav, MDBNavItem
 function NavbarPage(props) {
 
   return (
-      <MDBNav color="white" dark expand="md">
+      // <MDBNav color="white" dark expand="md">
        
-         <MDBNavbarNav left>
-            <MDBNavbarBrand href="/"> <strong id="name" class="paths">Isaac A.</strong> </MDBNavbarBrand>
-         </MDBNavbarNav>
+      //    <MDBNavbarNav left>
+      //       <MDBNavbarBrand href="/"> <strong id="name" class="paths">Isaac A.</strong> </MDBNavbarBrand>
+      //    </MDBNavbarNav>
 
 
          
 
           
          
-          <MDBNavItem>
-             <MDBNavLink to="/skills"><strong class="paths">Skills</strong></MDBNavLink>
-          </MDBNavItem>
+      //     <MDBNavItem>
+      //        <MDBNavLink to="/skills"><strong class="paths">Skills</strong></MDBNavLink>
+      //     </MDBNavItem>
 
-          {/* <MDBNavItem>
-             <MDBNavLink to="/" href="https://student.lvh.me:8080/users/edit"><strong>Skisasalls</strong></MDBNavLink>
-          </MDBNavItem> */}
+      //     {/* <MDBNavItem>
+      //        <MDBNavLink to="/" href="https://student.lvh.me:8080/users/edit"><strong>Skisasalls</strong></MDBNavLink>
+      //     </MDBNavItem> */}
 
-          <MDBNavItem id="nav-blog">
-            <a id="nav-blog" className="paths"href="https://github.com/IA26"><strong>Blog</strong></a>
-          </MDBNavItem>
+      //     <MDBNavItem id="nav-blog">
+      //       <a id="nav-blog" className="paths"href="https://github.com/IA26"><strong>Blog</strong></a>
+      //     </MDBNavItem>
 
-          <MDBNavItem>
-             <MDBNavLink to="/contact"><strong class="paths">Contact</strong></MDBNavLink>
-          </MDBNavItem>
+      //     <MDBNavItem>
+      //        <MDBNavLink to="/contact"><strong class="paths">Contact</strong></MDBNavLink>
+      //     </MDBNavItem>
 
          
            
-           {/* <MDBContainer>
-            <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/" className="social-icon hover">
-              <MDBIcon fab icon="linkedin-in" />
-            </a>
+      //      {/* <MDBContainer>
+      //       <a href="https://www.linkedin.com/in/isaac-sampson-avilez-423672197/" className="social-icon hover">
+      //         <MDBIcon fab icon="linkedin-in" />
+      //       </a>
            
-            <a href="https://github.com/IA26" className="social-icon hover" >
-              <MDBIcon fab icon="github" />
-            </a>
+      //       <a href="https://github.com/IA26" className="social-icon hover" >
+      //         <MDBIcon fab icon="github" />
+      //       </a>
 
-            <a href="https://isaac1180a.medium.com/" className="social-icon hover" >
-              <MDBIcon fab icon="medium-m" />
-            </a>
-          </MDBContainer> */}
+      //       <a href="https://isaac1180a.medium.com/" className="social-icon hover" >
+      //         <MDBIcon fab icon="medium-m" />
+      //       </a>
+      //     </MDBContainer> */}
           
-      </MDBNav>
+      // </MDBNav>
     );
 }
 
