03a109de3e283dc807e423fba113b592569fd884
Isaac <isaac1180@aol.com>
2021-03-31 19:30:35 -0400

    added skills navlink

--- src/components/NavBarPage.js
+++ src/components/NavBarPage.js
@@ -9,9 +9,11 @@ function NavbarPage(props) {
     <nav class="navbar navbar-expand-lg navbar-dark nav-custom"> 
      <a class="navbar-brand" href="#">Isaac Avilez</a>
       <div className="collapse navbar-collapse vertically-centered" id="navbarNav">
-       <ul className="navbar-nav">
-
-       </ul>
+        <ul className="navbar-nav">
+          <li className="nav-item" >
+           <NavLink to="/skills" className="nav-link">Skills</NavLink>
+          </li>
+        </ul>
       </div>
 
 
